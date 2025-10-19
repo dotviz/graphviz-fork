3ce2fdfee94947983f5223e4034c6571dcfa951b
vitalnodo <vitalnodo@outlook.com>
2025-10-19 16:19:14 +0300

    comment out gvusershape_size
    Co-authored-by: Ivan Goncharov <ivan.goncharov.ua@gmail.com>

--- lib/gvc/gvusershape.c
+++ lib/gvc/gvusershape.c
@@ -810,28 +810,32 @@ point gvusershape_size_dpi(usershape_t *us, pointf dpi) {
  * Loads user image from file name if not already loaded.
  * Return image size in points.
  */
-point gvusershape_size(graph_t *g, char *name) {
-  pointf dpi;
-  static char *oldpath;
+// point gvusershape_size(graph_t *g, char *name) {
+//   point rv;
+//   pointf dpi;
+//   static char *oldpath;
+//   usershape_t *us;
 
-  /* no shape file, no shape size */
-  if (!name || (*name == '\0')) {
-    return (point){.x = -1, .y = -1};
-  }
+//   /* no shape file, no shape size */
+//   if (!name || (*name == '\0')) {
+//     rv.x = rv.y = -1;
+//     return rv;
+//   }
 
-  if (!HTTPServerEnVar && (oldpath != Gvimagepath)) {
-    oldpath = Gvimagepath;
-    if (ImageDict) {
-      dtclose(ImageDict);
-      ImageDict = NULL;
-    }
-  }
+//   if (!HTTPServerEnVar && (oldpath != Gvimagepath)) {
+//     oldpath = Gvimagepath;
+//     if (ImageDict) {
+//       dtclose(ImageDict);
+//       ImageDict = NULL;
+//     }
+//   }
 
-  if ((dpi.y = GD_drawing(g)->dpi) >= 1.0)
-    dpi.x = dpi.y;
-  else
-    dpi.x = dpi.y = DEFAULT_DPI;
+//   if ((dpi.y = GD_drawing(g)->dpi) >= 1.0)
+//     dpi.x = dpi.y;
+//   else
+//     dpi.x = dpi.y = DEFAULT_DPI;
 
-  usershape_t *const us = gvusershape_open(name);
-  return gvusershape_size_dpi(us, dpi);
-}
+//   us = gvusershape_open(name);
+//   rv = gvusershape_size_dpi(us, dpi);
+//   return rv;
+// }
