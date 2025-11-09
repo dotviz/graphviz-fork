00ba87228ebfc4af6731a928360df0ad785597e1
vitalnodo <vitalnodo@outlook.com>
2025-11-09 19:20:22 +0200

    dejobify shapes.c callbacks

--- lib/common/types.h
+++ lib/common/types.h
@@ -175,13 +175,17 @@ typedef union inside_t {
 	pointf *vertices;	/* array of vertex points */
     } stroke_t;
 
+	typedef struct output_string_s output_string;
+	typedef struct SafeJob_s SafeJob;
+	typedef struct obj_state_s obj_state_t;
+	
     typedef struct shape_functions {	/* read-only shape functions */
 	void (*initfn) (node_t *);	/* initializes shape from node u.shape_info structure */
 	void (*freefn) (node_t *);	/* frees  shape from node u.shape_info structure */
 	 port(*portfn) (node_t *, char *, char *);	/* finds aiming point and slope of port */
 	 bool(*insidefn) (inside_t * inside_context, pointf);	/* clips incident gvc->e spline on shape of gvc->n */
 	int (*pboxfn)(node_t* n, port* p, int side, boxf rv[], int *kptr); /* finds box path to reach port */
-	void (*codefn) (GVJ_t * job, node_t * n);	/* emits graphics code for node */
+	void (*codefn) (output_string* output, SafeJob* safe_job, obj_state_t* obj, node_t * n);	/* emits graphics code for node */
     } shape_functions;
 
     typedef enum { SH_UNSET, SH_POLY, SH_RECORD, SH_POINT, SH_EPSF} shape_kind;
